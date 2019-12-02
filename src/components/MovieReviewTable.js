f15aaf861de08095d1cf3ea8f64d0670f5ff423e
nicholasstano <nicholasstano@gmail.com>
2019-12-02 15:03:44 -0500

    form started

--- src/components/MovieReviewTable.js
+++ src/components/MovieReviewTable.js
@@ -4,12 +4,32 @@ import 'react-table/react-table.css'
 
 export class MovieReviewTable extends Component {
 
-    state = { allMovieReviews: [] }
+    state = {
+        date: "",
+        title: "",
+        year: "",
+        director: "",
+        stars: "",
+        movie_img: "",
+        notes: ""
+    }
 
-    componentDidMount() {
-        fetch(`http://localhost:3000/movies`)
-            .then(res => res.json())
-            .then(movies => this.setState({ allMovieReviews: movies }))
+    handleMovieReviewChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleMovieReviewSubmit = event => {
+        event.preventDefault()
+        this.props.addNewMovieReview(this.state)
+        this.setState({
+            date: "",
+            title: "",
+            year: "",
+            director: "",
+            stars: "",
+            movie_img: "",
+            notes: ""
+        })
     }
 
     render() {
@@ -49,26 +69,26 @@ export class MovieReviewTable extends Component {
 
         return <div>
             <div className="movie-add-form">
-                <form>
-                    <label>Date<input type="text" name="date" placeholder="12-31-1999" />
+                <form onSubmit={this.handleMovieReviewSubmit}>
+                    <label>Date<input type="text" name="date" value={this.state.date} placeholder="12-31-1999" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Title<input type="text" name="title" placeholder="The Pink Panther" />
+                    <label>Title<input type="text" name="title" value={this.state.title} placeholder="The Pink Panther" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Year<input type="text" name="year" placeholder="1963" />
+                    <label>Year<input type="text" name="year" value={this.state.year} placeholder="1963" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Director<input type="text" name="director" placeholder="Blake Edwards" />
+                    <label>Director<input type="text" name="director" value={this.state.director} placeholder="Blake Edwards" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Stars<input type="text" name="stars" placeholder="333" />
+                    <label>Stars<input type="text" name="stars" value={this.state.stars} placeholder="333" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Poster<input type="text" name="poster" placeholder="https://upload.wikimedia.org/wikipedia/en/9/96/Pink_panther63.jpg" />
+                    <label>Poster<input type="text" name="movie_img" value={this.state.movie_img} placeholder="https://upload.wikimedia.org/wikipedia/en/9/96/Pink_panther63.jpg" onChange={this.handleMovieReviewChange} />
                     </label>
-                    <label>Notes<textarea name="notes" placeholder="Your notes for the recent viewing go here.........." />
+                    <label>Notes<textarea name="notes" value={this.state.notes} placeholder="Your notes for the recent viewing go here.........." onChange={this.handleMovieReviewChange} />
                     </label>
-                    <input type="submit" value="Submit" />
+                    <input type="submit" value="Add Movie Review" />
                 </form>
             </div><br />
             <ReactTable
-                data={this.state.allMovieReviews}
+                data={this.props.allMovieReviews}
                 columns={columns}
                 defaultSorted={[{ id: "id", desc: true }]}
             />
--- src/containers/MovieReviews.js
+++ src/containers/MovieReviews.js
@@ -3,11 +3,33 @@ import { withRouter } from 'react-router-dom'
 import MovieReviewTable from '../components/MovieReviewTable'
 
 export class MovieReviews extends Component {
+
+    state = { allMovieReviews: [] }
+
+    componentDidMount() {
+        fetch(`http://localhost:3000/movies`)
+            .then(res => res.json())
+            .then(movies => this.setState({ allMovieReviews: movies }))
+    }
+
+    addNewMovieReview = (movie) => {
+        console.log(movie)
+        this.setState({ allMovieReviews: [...this.state.allMovieReviews, movie] })
+        fetch(`http://localhost:3000/movies`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify(movie)
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Movie Reviews</h1>
-                <MovieReviewTable />
+                <MovieReviewTable allMovieReviews={this.state.allMovieReviews} addNewMovieReview={this.addNewMovieReview} />
             </div>
         )
     }
